d493398b48d2dbe5a7aa82c63ee58cb487a7d464
Ale Pantoja <pantojaalejandra1301@gmail.com>
2025-09-17 11:31:25 -0400

    New build

--- apps/backend/dist/server/entry.mjs
+++ apps/backend/dist/server/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_BNBmdDWB.mjs';
-import { manifest } from './manifest_B4LWplF6.mjs';
+import { manifest } from './manifest_BH_ua3qq.mjs';
 
 const serverIslandMap = new Map();;
 
